10328275335e086642557c4f39a808bdb5064dc0
Samarth Agarwal <btechsamarth@gmail.com>
2026-08-10 10:48:18 +0530

    Delete helpers directory

--- helpers/string_input_checker.py
+++
@@ -1,5 +0,0 @@
-def string_checker(temp):
-    if(len(temp.strip()) == 0):
-        print("                                                                              String Cant Be Empty")
-        return False
-    return True
