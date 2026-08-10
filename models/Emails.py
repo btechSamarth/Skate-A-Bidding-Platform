b3334670aa184ae615641f1882b2037139d3e703
Samarth Agarwal <btechsamarth@gmail.com>
2026-08-10 10:48:28 +0530

    Delete models directory

--- models/Emails.py
+++
@@ -1,8 +0,0 @@
-class EMAIL:
-
-    def __init__(self , Subject , Content, Sent_At , Sender_Email , Reciever_Email):
-        self.Subject = Subject
-        self.Content = Content
-        self.Sent_At = Sent_At
-        self.Sender_Email = Sender_Email
-        self.Reciever_Email = Reciever_Email
--- models/Product.py
+++
@@ -1,8 +0,0 @@
-class PRODUCT:
-    def __init__(self , Id , Seller_Id , Name , Price , Quantity, Category):
-        self.Id = Id
-        self.Seller_Id = Seller_Id
-        self.Name = Name
-        self.Price = Price
-        self.Quantity = Quantity
-        self.Category = Category
